097ed781fa0a498c65a15ab3f1f9f53d3655ef5b
orisenbazuru <orisenbazuru@gmail.com>
2022-02-21 17:02:21 +0100

    Updates

--- src/constants/mlmodel.ts
+++
@@ -1,7 +0,0 @@
-export const mlmodel_name = "EfficientNet"
-export const mlmodel_link = "https://rwightman.github.io/pytorch-image-models/models/#efficientnet-efficientnetpy"
-export const mlmodel_github = "https://github.com/rwightman/pytorch-image-models/blob/master/timm/models/efficientnet.py"
-export const dataset_name = "ImageNet-Sketch"
-export const dataset_link = "https://github.com/HaohanWang/ImageNet-Sketch"
-export const num_images_perclass = 5
-export const dataset_publink = "https://drive.switch.ch/index.php/s/cYJCw5ftRbISodw/download?path=%2F"
--- src/constants/settings.ts
+++
@@ -1,5 +0,0 @@
-export const MAX_WORD_LENGTH = 5 // placeholder for max_word_length as this will change based on the chosen label
-export const MAX_CHALLENGES = 6
-export const ALERT_TIME_MS = 2000
-export const REVEAL_TIME_MS = 350
-export const GAME_LOST_INFO_DELAY = (MAX_WORD_LENGTH + 1) * REVEAL_TIME_MS
